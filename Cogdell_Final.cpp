33c560239c9802d1ee2bd2e572f6f60646459a45
bmcogdel <bmcogdel@my.loyno.edu>
2021-05-04 17:09:13 -0400

    commit

---
+++ Cogdell_Final.cpp
@@ -0,0 +1,59 @@
+// Objective was to play music using C++ code
+// B. Cogdell  05/04/2021
+
+#include <iostream>>
+#include<windows.h>
+
+using namespace std;
+int main() 
+{
+
+int F;
+int T;
+
+cout << "Frequency : ";
+cin >> F;
+system("cin");
+cout <<"Time : ";
+cin >> T;
+system("");
+
+Beep(F,T);
+
+    Beep(314, 2000);
+    Beep(314, 1400);
+    Beep(467, 1400);
+    Beep(526, 1400);
+    Beep(526, 1400);
+    Beep(624, 1400):
+
+    Beep(314, 500);
+    Beep(314, 500);
+    Beep(467, 500);
+    Beep(526, 500);
+    Beep(526, 500);
+    Beep(624, 500);
+
+    Beep(467, 500);
+    Beep(314, 500);
+    Beep(467, 500);
+    Beep(526, 500);
+    Beep(526, 500);
+    Beep(624, 500);
+
+    Beep(314, 2000);
+    Beep(314, 1400);
+    Beep(467, 1400);
+    Beep(526, 1400);
+    Beep(526, 1400);
+    Beep(624, 1400):
+
+    Beep(467, 500);
+    Beep(314, 500);
+    Beep(467, 500);
+    Beep(526, 500);
+    Beep(526, 500);
+    Beep(624, 500);
+
+    return 0;
+}
